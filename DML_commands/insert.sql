795eca42ade958d33a77f44d4c173c8782790e13
sanju1198 <gurramsaiganesh93@gmail.com>
2025-01-30 11:24:24 +0530

    wrote dml commands

---
+++ DML_commands/insert.sql
@@ -0,0 +1,16 @@
+/*
+    Definition:
+        the insert command in SQL is used to insert new records into a table.
+
+    Syntax:
+        INSERT INTO table_name (column1, column2, column3, ...)
+        VALUES (value1, value2, value3, ...);
+
+    Uses:
+        - Insert new records into a table.
+        - Add data to a table from another table.
+        - Insert default values into a table.
+        - Insert data into specific columns of a table.
+*/
+
+insert into Users (user_id, username, email, phone_number);
